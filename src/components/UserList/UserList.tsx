3c8b5c71b40c14c48ce0743c8ca29cb18e610461
emieldecraemer <emiel.decraemer@student.tsaam.be>
2026-03-31 12:04:27 +0200

    laatste push

--- src/components/UserList/UserList.tsx
+++ src/components/UserList/UserList.tsx
@@ -6,11 +6,21 @@ interface Props {
 }
 
 export default function UserList({ users, registeredUsers }: Props) {
+
     return <>
     <h1>Geregistreerde Gebruikers: </h1>
     <ul>
+        {/* niet de juiste opl */}
         {users.map((user) => (
-           <li>{user}</li> 
+           <li className='geregistreerd'>{user} (geregistreerd)</li> 
+        ))}
+        {/* werkt niet maar is dichter bij een oplossing. om geen error te geven staat het in comment. */}
+        {users.map((user) => (
+            {(user == registeredUsers.find(user)) ? (
+                <li className='geregistreerd'>{user} (Geregistreerde gebruiker)</li> 
+            ) : (
+                <li className='ongeregistreerd'>{user} (Ongeregistreerde gebruiker)</li>
+            )}
         ))}
     </ul>
     </>
